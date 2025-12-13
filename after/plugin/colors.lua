78757179197b368c82922bbef86b7c40154afe60
Kumai <k.hodaifa@edu.rptu.de>
2025-12-13 20:46:18 +0100

    add new theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,10 +1,10 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+	color = color or "tokyonight-storm"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "Visual", { bg = "#3e4452", fg = "#ffffff", bold = true })
+	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Visual", { bg = "#5c6370", fg = "#ffffff", bold = true })
 end
 
 ColorMyPencils()
