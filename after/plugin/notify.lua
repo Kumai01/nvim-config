93feb2597b6c0edf2cee57c0a2d134f93d914777
Kumai <k.hodaifa@edu.rptu.de>
2025-12-14 14:29:09 +0100

    dismiss notifications

--- after/plugin/notify.lua
+++ after/plugin/notify.lua
@@ -1,5 +1,11 @@
-require("notify").setup({
+local notify = require("notify")
+
+notify.setup({
   background_colour = "#1e1e2e",  -- match your colorscheme background
   merge_duplicates = true,
 })
+
+vim.keymap.set("n", "<leader>nd", function()
+    notify.dismiss({ silent = true, pending = true })
+end, { desc = "Dismiss notifications" } )
 
