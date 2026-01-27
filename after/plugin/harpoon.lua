da0682c3f7b39d9a45056435201c94fee80f8efa
Kumai <k.hodaifa@edu.rptu.de>
2026-01-27 10:34:52 +0100

    add desc to harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,14 +5,14 @@ harpoon:setup()
 -- local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-vim.keymap.set("n", "<leader>m", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, {desc="add file to harpoon list"})
+vim.keymap.set("n", "<leader>m", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, {desc="add file to harpoon list"})
 
 for i = 1, 4 do
-    vim.keymap.set("n", "<leader>" .. i, function() harpoon:list():select(i) end)
+    vim.keymap.set("n", "<leader>" .. i, function() harpoon:list():select(i) end, {desc="go to file in harpoon list"})
 end
 
--- Toggle previous & next buffers stored within Harpoon list (the shortcuts don't work)
-vim.keymap.set("n", "<Tab>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<S-Tab>", function() harpoon:list():next() end)
+-- Toggle previous & next buffers stored within Harpoon list 
+vim.keymap.set("n", "<leader><Tab>", function() harpoon:list():prev() end, {desc="go to previous file in harpoon list"})
+vim.keymap.set("n", "<leader><S-Tab>", function() harpoon:list():next() end, {desc="go to next file in harpoon list"})
 
