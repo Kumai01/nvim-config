c8fdc3ce7b2c7aadab07ba8f6fbec3bef3bf86f3
Kumai <k.hodaifa@edu.rptu.de>
2026-01-31 11:35:17 +0100

    remove remaping

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -61,9 +61,9 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
+-- vim.keymap.set("n", "<leader><leader>", function()
+--     vim.cmd("so")
+-- end)
 
 vim.keymap.set("n", "<F5>", function()
   local ft = vim.bo.filetype
