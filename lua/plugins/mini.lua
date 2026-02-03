83f9dcc61d6d0fb4bf54acd69e4afe0b76a0426a
Kumai <k.hodaifa@edu.rptu.de>
2026-02-03 09:58:13 +0100

    config mini and mini statusline

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,7 +1,21 @@
+local require_all = function(directory)
+    -- Get the list of all .lua files in the specified directory
+    -- Use 'lua/' .. directory .. '/*.lua' to search within the Neovim lua path
+    local base = vim.fn.stdpath("config") .. "/lua/" .. directory
+    local files = vim.fn.globpath(base, "*.lua", false, true)
+
+    for _, file in ipairs(files) do
+        -- Extract the filename without the path and extension
+        -- e.g., ".../nvim/lua/mini/statusline.lua" -> "statusline"
+        local module_name = vim.fn.fnamemodify(file, ':t:r')
+        require(directory .. '.' .. module_name)
+    end
+end
+
 return {
     'nvim-mini/mini.nvim',
     version = false,
     config = function ()
-        require('mini.statusline').setup()
+        require_all('plugins/mini')
     end
 }
